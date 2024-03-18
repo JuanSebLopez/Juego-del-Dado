9576a0988b093b5d702b208c06958f161c1a8d5b
Juan Lopez <jslopezmarquez@unicesar.edu.co>
2024-03-18 15:06:40 -0500

    src para img2 modificado para su correcta visualizacion

--- script.js
+++ script.js
@@ -32,22 +32,22 @@ function pickImg1(number1){
 function pickImg2(number2){
     switch (number2) {
         case 1:
-            document.querySelector(".img2").src = "/images/dice1.png";
+            document.querySelector(".img2").src = "images/dice1.png";
             break;
         case 2:
-            document.querySelector(".img2").src = "/images/dice2.png";
+            document.querySelector(".img2").src = "images/dice2.png";
             break;
         case 3:
-            document.querySelector(".img2").src = "/images/dice3.png";
+            document.querySelector(".img2").src = "images/dice3.png";
             break;
         case 4:
-            document.querySelector(".img2").src = "/images/dice4.png";
+            document.querySelector(".img2").src = "images/dice4.png";
             break;
         case 5:
-            document.querySelector(".img2").src = "/images/dice5.png";
+            document.querySelector(".img2").src = "images/dice5.png";
             break;
         case 6:
-            document.querySelector(".img2").src = "/images/dice6.png";
+            document.querySelector(".img2").src = "images/dice6.png";
             break;
     }
 }
