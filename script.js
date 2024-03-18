5bebbdac354f0ff77a86bee886894a0ed134be6d
Juan Lopez <jslopezmarquez@unicesar.edu.co>
2024-03-18 14:58:52 -0500

    rutas modificadas

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ winner(number, number2);
 function pickImg1(number1){
     switch (number1) {
         case 1:
-            document.querySelector(".img1").src = "/images/dice1.png";
+            document.querySelector(".img1").src = "images/dice1.png";
             break;
         case 2:
-            document.querySelector(".img1").src = "/images/dice2.png";
+            document.querySelector(".img1").src = "images/dice2.png";
             break;
         case 3:
-            document.querySelector(".img1").src = "/images/dice3.png";
+            document.querySelector(".img1").src = "images/dice3.png";
             break;
         case 4:
-            document.querySelector(".img1").src = "/images/dice4.png";
+            document.querySelector(".img1").src = "images/dice4.png";
             break;
         case 5:
-            document.querySelector(".img1").src = "/images/dice5.png";
+            document.querySelector(".img1").src = "images/dice5.png";
             break;
         case 6:
-            document.querySelector(".img1").src = "/images/dice6.png";
+            document.querySelector(".img1").src = "images/dice6.png";
             break;
     }
 }
